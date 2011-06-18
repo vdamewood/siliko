0133d015712041d7cb4b4cbb56788dd52562a468
Vincent Damewood <git@damewood.us>
2011-06-18 09:37:07 +0000

    Updated ctypes-based Python bindings.

--- ct.py
+++ ct.py
@@ -1,8 +1,41 @@
 #!/usr/bin/env python
 
-from ctypes import *
-from sys import argv
+import ctypes as _ctypes
+
+_clib = _ctypes.CDLL("libvldc.so")
+_clib.vldc_error_message.restype = _ctypes.c_char_p
+
+def vldc(inString):
+	rValue = _clib.vldc(inString)
+	state = _clib.vldc_error_status()
+	exception_message = None
+	if state == 1:
+		exception_message = "Error making calculation. Also can't get error message."
+	if state == 2:
+		exception_message = _clib.vldc_error_message()
+	_clib.vldc_clean()
+	if state != 0:
+		raise Exception(exception_message)
+	return rValue
 
 if __name__ == "__main__":
-	libvldc = CDLL("libvldc.so")
-	print "Value:", libvldc.vldc(argv[1])
+	from sys import argv, stdin
+	if len(argv) == 2:
+		try:
+			print "Value:", vldc(argv[1])
+		except Exception as e:
+			print "Error:", e
+	elif len(argv) == 1:
+		keeplooping = True
+		istty = stdin.isatty()
+		while keeplooping:
+			try:
+				if istty: print ">",
+				value = vldc(raw_input())
+				print "Value:", value
+			except EOFError as e:
+				print
+				keeplooping = False
+			except Exception as e:
+				print "Error:", e
+
