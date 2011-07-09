3a6b7f6e46b2b0ec6ea644d9f0e0df5941c2e6fd
Vincent Damewood <git@damewood.us>
2011-07-09 16:08:03 +0000

    Removed old operator code.

--- libxvcalc/functions.c
+++ libxvcalc/functions.c
@@ -1,4 +1,7 @@
+#include <stdlib.h>
 #include <string.h>
+#include <time.h>
+
 #include "xvcalc.h"
 #include "functions.h"
 
--- libxvcalc/tree.c
+++ libxvcalc/tree.c
@@ -4,7 +4,6 @@
 
 #include "xvcalc.h"
 #include "tree.h"
-#include "operators.h"
 #include "functions.h"
 #include "xvcalcix.h"
 
