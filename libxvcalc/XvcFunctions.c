077ab146922ed7f8f0421a4060c078649e462afb
Vincent Damewood <git@damewood.us>
2012-01-06 08:43:17 -0800

    Added a sqrt() function.

--- libxvcalc/XvcFunctions.c
+++ libxvcalc/XvcFunctions.c
@@ -19,6 +19,30 @@ static number xvcalc_func_abs(int argc, number * argv, jmp_buf jb)
 	return rVal;
 }
 
+static number xvcalc_func_sqrt(int argc, number * argv, jmp_buf jb)
+{
+	number rVal;
+	float inVal;
+	if (argc != 1) longjmp(jb, E_ARGUMENTS);
+	
+	if (
+		(argv[0].type == 'i' && argv[0].i < 0)
+		|| (argv[0].type == 'f' && argv[0].f < 0.0)) {
+		longjmp(jb, E_DOMAIN);
+	}
+	
+	if (argv[0].type == 'f') {
+		inVal = argv[0].f;
+	}
+	else {
+		inVal = (float) argv[0].i;
+	}
+	rVal.type = 'f';
+	rVal.f = sqrt(inVal);
+	
+	return rVal;
+}
+
 // This will be removed when more functions are implemented.
 static number xvcalc_func_dummy(int argc, number * argv, jmp_buf jb)
 {
@@ -31,6 +55,8 @@ static function_ptr xvcalc_get_function(const char * name)
 {
 	if (strcmp(name, "abs") == 0)
 		return xvcalc_func_abs;
+	else if (strcmp(name, "sqrt") == 0)
+		return xvcalc_func_sqrt;
 	else if (strcmp(name, "dummy") == 0)
 		return xvcalc_func_dummy;
 	else
