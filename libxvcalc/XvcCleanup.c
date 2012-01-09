a21d7712961d1514228d0b98407298c7ee6da0e1
Vincent Damewood <git@damewood.us>
2012-01-09 10:48:24 -0800

    Code cleanup.

--- libxvcalc/XvcCleanup.c
+++ libxvcalc/XvcCleanup.c
@@ -6,19 +6,9 @@
 #include "XvcFunctionId.h"
 #include "XvcTree.h"
 
-static void debug_report(const char * function, void * address)
-{
-	if (!address) {
-		printf("[NULL]:      %s\n", function);
-	}
-	else {
-		printf("%p: %s\n", address, function);
-	}
-}
-
 struct TreeList {
 	struct TreeList * next;
-	XvcTree * tree;
+	XvcTree * item;
 };
 
 struct ArglistList { // Yeah, I know.
@@ -36,10 +26,6 @@ static struct TreeList * DanglingTrees = NULL;
 static struct ArglistList * DanglingArglists = NULL;
 static struct IdList * DanglingIds = NULL;
 
-void print_tree_list();
-void print_arglist_list();
-void print_id_list();
-
 void XvcCleanupClearAll()
 {
 	XvcCleanupClearTrees();
@@ -47,50 +33,16 @@ void XvcCleanupClearAll()
 	XvcCleanupClearFunctionIds();
 }
 
-void print_tree_list()
-{
-	struct TreeList * CurrentNode;
-	CurrentNode = DanglingTrees;
-	while (CurrentNode) {
-		printf("%p: Cached Tree\n", CurrentNode->tree);
-		CurrentNode = CurrentNode->next;
-	}
-}
-
-void print_arglist_list()
-{
-	struct ArglistList * CurrentNode;
-	CurrentNode = DanglingArglists;
-	while (CurrentNode) {
-		printf("%p: Cached Arglist\n", CurrentNode->item);
-		CurrentNode = CurrentNode->next;
-	}
-}
-
-void print_id_list()
-{
-	struct IdList * CurrentNode;
-	CurrentNode = DanglingIds;
-	while (CurrentNode) {
-		printf("%p: Cached FunctionId\n", CurrentNode->item);
-		CurrentNode = CurrentNode->next;
-	}
-}
-
-
 void XvcCleanupCacheTree(XvcTree * in)
 {
-	//debug_report(__FUNCTION__, in);
 	struct TreeList * NewNode;
 	struct TreeList * CurrentNode;
 	NewNode = malloc(sizeof(struct TreeList));
-	NewNode->tree = in;
+	NewNode->item = in;
 	NewNode->next = NULL;
-	//printf("NewNode: %p t:%p n:%p\n", NewNode, NewNode->tree, NewNode->next);
 
 	if (!DanglingTrees) {
 		DanglingTrees = NewNode;
-		//printf("!DT so DT becomes: %p\n", DanglingTrees);
 	}
 	else {
 		CurrentNode = DanglingTrees;
@@ -98,29 +50,23 @@ void XvcCleanupCacheTree(XvcTree * in)
 			CurrentNode = CurrentNode->next;
 		}
 		CurrentNode->next = NewNode;
-		//printf("Appending %p to %p.\n", NewNode, CurrentNode);
 	}
-	//print_tree_list();
 }
 
 void XvcCleanupReleaseTree(XvcTree * in)
 {
-	//debug_report(__FUNCTION__, in);
 	struct TreeList ** ParentPointer;
 	struct TreeList * CurrentNode;
 
 	if (!DanglingTrees) {
-		//printf("!Dangling Tree, so not removing anything.\n");
 		return;
 	}
 
 	CurrentNode = DanglingTrees;
 	ParentPointer = &DanglingTrees;
 	
-	//printf("ParentPointer(%p), CurrentNode(%p)\n", ParentPointer, DanglingTrees);
 	while (CurrentNode) {
-		if (CurrentNode->tree == in) {
-			//printf("Found %p in %p. Deleting!\n", in, CurrentNode);
+		if (CurrentNode->item == in) {
 			*ParentPointer = CurrentNode->next;
 			free(CurrentNode);
 			CurrentNode = *ParentPointer;
@@ -134,7 +80,6 @@ void XvcCleanupReleaseTree(XvcTree * in)
 
 void XvcCleanupClearTrees()
 {
-	//debug_report(__FUNCTION__, NULL);
 	struct TreeList * CurrentNode;
 	struct TreeList * OldNode;
 
@@ -142,8 +87,7 @@ void XvcCleanupClearTrees()
 	DanglingTrees = NULL;
 	
 	while (CurrentNode) {
-		//printf("Deleting: %p t:%p n:%p\n", CurrentNode, CurrentNode->tree, CurrentNode->next);
-		XvcTreeDelete(CurrentNode->tree);
+		XvcTreeDelete(CurrentNode->item);
 		OldNode = CurrentNode;
 		CurrentNode = CurrentNode->next;
 		free(OldNode);
@@ -154,17 +98,14 @@ void XvcCleanupClearTrees()
 
 void XvcCleanupCacheArglist(XvcArglist * in)
 {
-	//debug_report(__FUNCTION__, in);
 	struct ArglistList * NewNode;
 	struct ArglistList * CurrentNode;
 	NewNode = malloc(sizeof(struct ArglistList));
 	NewNode->item = in;
 	NewNode->next = NULL;
-	//printf("NewNode: %p t:%p n:%p\n", NewNode, NewNode->item, NewNode->next);
 
 	if (!DanglingArglists) {
 		DanglingArglists = NewNode;
-		//printf("!DT so DT becomes: %p\n", DanglingTrees);
 	}
 	else {
 		CurrentNode = DanglingArglists;
@@ -172,29 +113,23 @@ void XvcCleanupCacheArglist(XvcArglist * in)
 			CurrentNode = CurrentNode->next;
 		}
 		CurrentNode->next = NewNode;
-		//printf("Appending %p to %p.\n", NewNode, CurrentNode);
 	}
-	//print_tree_list();
 }
 
 void XvcCleanupReleaseArglist(XvcArglist * in)
 {
-	//debug_report(__FUNCTION__, in);
 	struct ArglistList ** ParentPointer;
 	struct ArglistList * CurrentNode;
 
 	if (!DanglingArglists) {
-		//printf("!Dangling Tree, so not removing anything.\n");
 		return;
 	}
 
 	CurrentNode = DanglingArglists;
 	ParentPointer = &DanglingArglists;
 	
-	//printf("ParentPointer(%p), CurrentNode(%p)\n", ParentPointer, DanglingTrees);
 	while (CurrentNode) {
 		if (CurrentNode->item == in) {
-			//printf("Found %p in %p. Deleting!\n", in, CurrentNode);
 			*ParentPointer = CurrentNode->next;
 			free(CurrentNode);
 			CurrentNode = *ParentPointer;
@@ -208,7 +143,6 @@ void XvcCleanupReleaseArglist(XvcArglist * in)
 
 void XvcCleanupClearArglists()
 {
-	//debug_report(__FUNCTION__, NULL);
 	struct ArglistList * CurrentNode;
 	struct ArglistList * OldNode;
 
@@ -216,7 +150,6 @@ void XvcCleanupClearArglists()
 	DanglingArglists = NULL;
 	
 	while (CurrentNode) {
-		//printf("Deleting: %p t:%p n:%p\n", CurrentNode, CurrentNode->tree, CurrentNode->next);
 		XvcArglistDelete(CurrentNode->item);
 		OldNode = CurrentNode;
 		CurrentNode = CurrentNode->next;
@@ -228,17 +161,14 @@ void XvcCleanupClearArglists()
 
 void XvcCleanupCacheFunctionId(char * in)
 {
-	//debug_report(__FUNCTION__, in);
 	struct IdList * NewNode;
 	struct IdList * CurrentNode;
 	NewNode = malloc(sizeof(struct IdList));
 	NewNode->item = in;
 	NewNode->next = NULL;
-	//printf("NewNode: %p t:%p n:%p\n", NewNode, NewNode->item, NewNode->next);
 
 	if (!DanglingIds) {
 		DanglingIds = NewNode;
-		//printf("!DT so DT becomes: %p\n", DanglingTrees);
 	}
 	else {
 		CurrentNode = DanglingIds;
@@ -246,29 +176,23 @@ void XvcCleanupCacheFunctionId(char * in)
 			CurrentNode = CurrentNode->next;
 		}
 		CurrentNode->next = NewNode;
-		//printf("Appending %p to %p.\n", NewNode, CurrentNode);
 	}
-	//print_tree_list();
 }
 
 void XvcCleanupReleaseFunctionId(char * in)
 {
-	//debug_report(__FUNCTION__, in);
 	struct IdList ** ParentPointer;
 	struct IdList * CurrentNode;
 
 	if (!DanglingIds) {
-		//printf("!Dangling Tree, so not removing anything.\n");
 		return;
 	}
 
 	CurrentNode = DanglingIds;
 	ParentPointer = &DanglingIds;
 	
-	//printf("ParentPointer(%p), CurrentNode(%p)\n", ParentPointer, DanglingTrees);
 	while (CurrentNode) {
 		if (CurrentNode->item == in) {
-			//printf("Found %p in %p. Deleting!\n", in, CurrentNode);
 			*ParentPointer = CurrentNode->next;
 			free(CurrentNode);
 			CurrentNode = *ParentPointer;
@@ -282,7 +206,6 @@ void XvcCleanupReleaseFunctionId(char * in)
 
 void XvcCleanupClearFunctionIds()
 {
-	//debug_report(__FUNCTION__, NULL);
 	struct IdList * CurrentNode;
 	struct IdList * OldNode;
 
@@ -290,7 +213,6 @@ void XvcCleanupClearFunctionIds()
 	DanglingIds = NULL;
 	
 	while (CurrentNode) {
-		//printf("Deleting: %p t:%p n:%p\n", CurrentNode, CurrentNode->tree, CurrentNode->next);
 		XvcFunctionIdDelete(CurrentNode->item);
 		OldNode = CurrentNode;
 		CurrentNode = CurrentNode->next;
