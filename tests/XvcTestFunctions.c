f94061a116e148408c2d07bfee2ca8b6896e4e04
Vincent Damewood <git@damewood.us>
2012-01-27 13:19:59 -0800

    Fixed a test to match recent changes to the core library.

--- tests/XvcTestFunctions.c
+++ tests/XvcTestFunctions.c
@@ -17,34 +17,36 @@
  */
 
 #include <stdio.h>
-#include <setjmp.h>
 #include "XvcFunctionCall.h"
+
+static int IsNumber(XvcNumber n) {
+	return n.status == S_INTEGER || n.status == S_FLOAT;
+}
 
 int main()
 {
-	jmp_buf oops;     // Required for catching exceptions.
 	XvcNumber inputs[1]; // Arguments to functions.
 	XvcNumber value;     // Return value of function.
 	int total = 0;    // Total number of tests run.
 	int success = 0;  // Number of successful tests.
 
+	////////////////////////////////////////////////////////////////////
+	
 	printf("Test 1: abs(-400) returns 400... ");
-	inputs[0].type = 'i';
+	inputs[0].status = S_INTEGER;
 	inputs[0].i = -400;
 	total++;
-	if(setjmp(oops) == 0) {
-		value = XvcFunctionCall("abs", 1, inputs, oops);
-		if (value.type == 'i' && value.i == 400) {
-			printf("success.\n");
-			success++;
-		}
-		else {
-			printf("failed. Value returned was: %c %i %f",
-				   value.type, value.i, value.f);
-		}
+	value = XvcFunctionCall("abs", 1, inputs);
+	if (value.status == S_INTEGER && value.i == 400) {
+		printf("success.\n");
+		success++;
+	}
+	else if (value.status == S_FLOAT) {
+		printf("failed. Value returned was: %c %i %f",
+			   value.status, value.i, value.f);
 	}
 	else {
-		printf("failed. Called longjmp().\n");
+		printf("failed. Error code returned.\n");
 	}
 
 	////////////////////////////////////////////////////////////////////
