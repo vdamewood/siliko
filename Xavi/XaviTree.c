b300f47dc06f29ab4137ddd242a2e2b3ee29ade6
Vincent Damewood <git@damewood.us>
2014-05-26 17:46:21 -0700

    Removed commented-out code.

--- Xavi/XaviTree.c
+++ Xavi/XaviTree.c
@@ -312,7 +312,7 @@ int XaviTreePushFront(XaviTreeNode *mainBranch, XaviTreeNode *newNode)
 
 	newListNode->next = mainBranch->branch->list;
 	mainBranch->branch->list = newListNode;
-	
+
 	return 1;
 }
 
@@ -333,61 +333,6 @@ int XaviTreePush(XaviTreeNode *mainNode, XaviTreeNode *newNode)
 
 	newListNode->next = NULL;
 	currentListNode->next = newListNode;
-	
+
 	return 1;
 }
-
-
-/*
-XaviArglist *XaviArglistNew(XaviTreeNode *NewTree, XaviArglist *OldList)
-{
-	XaviArglist *rVal;
-
-	if ((rVal = malloc(sizeof(XaviArglist))))
-	{
-		if (OldList)
-			rVal->depth = OldList->depth + 1;
-		else
-			rVal->depth = 1;
-		rVal->value = NewTree;
-		rVal->next = OldList;
-	}
-	return rVal;
-}
-
-void XaviArglistDelete(XaviArglist *OldArglist)
-{
-	if (OldArglist)
-	{
-		XaviArglistDelete(OldArglist->next);
-		XaviTreeDelete(OldArglist->value);
-		free(OldArglist);
-	}
-}
-
-XaviTreeNode **XaviArglistGetTrees(XaviArglist *InArglist)
-{
-	XaviTreeNode **rVal;
-	XaviArglist *Current = InArglist;
-	XaviArglist *next;
-	int i;
-	int depth = InArglist->depth;
-
-	if ((rVal = malloc(depth * sizeof(XaviTreeNode *))))
-	{
-		for (i = 0; i < depth; i++)
-		{
-			rVal[i] = Current->value;
-			next = Current->next;
-			free(Current);
-			Current = next;
-		}
-	}
-	else
-	{
-		rVal = NULL;
-	}
-
-	return rVal;
-}
-*/
