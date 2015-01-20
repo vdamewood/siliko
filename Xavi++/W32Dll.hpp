2223db2847e7912a59270b9267c37b3d182ef190
Vincent Damewood <git@damewood.us>
2015-01-20 01:03:22 -0800

    Fixed problems building on Windows.

--- Xavi++/W32Dll.hpp
+++ Xavi++/W32Dll.hpp
@@ -1,13 +1,13 @@
 #if !defined XAVI_W32_DLL
 
 #if defined _WIN32 && defined _MSC_VER
-#  if defined Xavi_EXPORTS
+#  if defined Xavipp_EXPORTS
 #    define XAVI_API __declspec(dllexport)
 #    define XAVI_EXTERN
-#  else // Xavi_EXPORTS
+#  else // Xavipp_EXPORTS
 #    define XAVI_API __declspec(dllimport)
 #    define XAVI_EXTERN extern
-#  endif // Xavi_EXPORTS
+#  endif // Xavipp_EXPORTS
 #else // _WIN32 && __MSC_VER
 #  define XAVI_API
 #  define XAVI_EXTERN
