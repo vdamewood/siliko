997de73326a34ac3bdad7e4e67da8117b641fd7e
Vincent Damewood <git@damewood.us>
2015-02-15 19:55:16 -0800

    Edited Cocoa-based skeleton to actually use Xavi.

--- Xavi-Gui-Cocoa/Delegate.m
+++ Xavi-Gui-Cocoa/Delegate.m
@@ -1,11 +1,16 @@
 #import "Delegate.h"
 
+#include <Xavi/StringSource.h>
+#include <Xavi/InfixParser.h>
+#include <Xavi/SyntaxTree.h>
+#include <Xavi/FunctionCaller.h>
+
 @implementation XAVIDelegate
 
 
 - (void)applicationDidFinishLaunching:(NSNotification *)aNotification
 {
-	[self.output setStringValue: @"Started"];
+	XaviFunctionCallerSetUp();
 }
 
 - (BOOL)applicationShouldTerminateAfterLastWindowClosed:(NSApplication *)theApplication
@@ -13,14 +18,49 @@
 	return YES;
 }
 
-/*
 - (void)applicationWillTerminate:(NSNotification *)aNotification
 {
+	XaviFunctionCallerTearDown();
 }
-*/
 
 - (IBAction) Calculate:(id)sender
 {
-	[self.output setStringValue: @"Test"];
+	XaviSyntaxTreeNode *Ast;
+	XaviValue Result;
+
+	Ast = XaviParseInfix(XaviStringSourceNew([[self.input stringValue] UTF8String]));
+	Result = XaviSyntaxTreeEvaluate(Ast);
+	XaviSyntaxTreeDelete(Ast);
+
+	switch (Result.Status)
+	{
+	case XAVI_VAL_INTEGER:
+		[self.output setIntegerValue: Result.Integer];
+		break;
+	case XAVI_VAL_FLOAT:
+		[self.output setFloatValue: Result.Float];
+		break;
+        case XAVI_VAL_MEMORY_ERR:
+		[self.output setStringValue: @"Out of memory"];
+		break;
+        case XAVI_VAL_SYNTAX_ERR:
+		[self.output setStringValue: @"Syntax error"];
+		break;
+        case XAVI_VAL_ZERO_DIV_ERR:
+		[self.output setStringValue: @"Division by zero"];
+		break;
+        case XAVI_VAL_BAD_FUNCTION:
+		[self.output setStringValue: @"Function not found"];
+		break;
+        case XAVI_VAL_BAD_ARGUMENTS:
+		[self.output setStringValue: @"Bad arguments"];
+		break;
+        case XAVI_VAL_DOMAIN_ERR:
+		[self.output setStringValue: @"Domain error"];
+		break;
+        case XAVI_VAL_RANGE_ERR:
+		[self.output setStringValue: @"Range error"];
+		break;
+	}
 }
 @end
