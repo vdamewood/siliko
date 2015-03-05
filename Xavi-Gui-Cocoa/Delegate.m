47a13e65968122332b10a79c728bf55a3833fe46
Vincent Damewood <git@damewood.us>
2015-03-04 18:56:52 -0800

    Fixed up recent changes to work with OS X.

--- Xavi-Gui-Cocoa/Delegate.m
+++ Xavi-Gui-Cocoa/Delegate.m
@@ -32,35 +32,9 @@
 	Result = XaviSyntaxTreeEvaluate(Ast);
 	XaviSyntaxTreeDelete(Ast);
 
-	switch (Result.Status)
-	{
-	case XAVI_VAL_INTEGER:
-		[self.output setIntegerValue: Result.Integer];
-		break;
-	case XAVI_VAL_FLOAT:
-		[self.output setFloatValue: Result.Float];
-		break;
-        case XAVI_VAL_MEMORY_ERR:
-		[self.output setStringValue: @"Out of memory"];
-		break;
-        case XAVI_VAL_SYNTAX_ERR:
-		[self.output setStringValue: @"Syntax error"];
-		break;
-        case XAVI_VAL_ZERO_DIV_ERR:
-		[self.output setStringValue: @"Division by zero"];
-		break;
-        case XAVI_VAL_BAD_FUNCTION:
-		[self.output setStringValue: @"Function not found"];
-		break;
-        case XAVI_VAL_BAD_ARGUMENTS:
-		[self.output setStringValue: @"Bad arguments"];
-		break;
-        case XAVI_VAL_DOMAIN_ERR:
-		[self.output setStringValue: @"Domain error"];
-		break;
-        case XAVI_VAL_RANGE_ERR:
-		[self.output setStringValue: @"Range error"];
-		break;
-	}
+	char *ResultCString = XaviValueToString(Result);
+	NSString *ResultString = [[NSString alloc] initWithUTF8String: ResultCString];
+	free(ResultCString);
+	[self.output setStringValue: ResultString];
 }
 @end
--- XaviPP-Gui-Cocoa/Delegate.mm
+++ XaviPP-Gui-Cocoa/Delegate.mm
@@ -36,35 +36,9 @@
 	Result = Ast->Evaluate();
 	delete Ast;
 
-	switch (Result.Status())
-	{
-	case Xavi::Value::INTEGER:
-		[self.output setIntegerValue: Result.Integer()];
-		break;
-	case Xavi::Value::FLOAT:
-		[self.output setFloatValue: Result.Float()];
-		break;
-        case Xavi::Value::MEMORY_ERR:
-		[self.output setStringValue: @"Out of memory"];
-		break;
-        case Xavi::Value::SYNTAX_ERR:
-		[self.output setStringValue: @"Syntax error"];
-		break;
-        case Xavi::Value::ZERO_DIV_ERR:
-		[self.output setStringValue: @"Division by zero"];
-		break;
-        case Xavi::Value::BAD_FUNCTION:
-		[self.output setStringValue: @"Function not found"];
-		break;
-        case Xavi::Value::BAD_ARGUMENTS:
-		[self.output setStringValue: @"Bad arguments"];
-		break;
-        case Xavi::Value::DOMAIN_ERR:
-		[self.output setStringValue: @"Domain error"];
-		break;
-        case Xavi::Value::RANGE_ERR:
-		[self.output setStringValue: @"Range error"];
-		break;
-	}
+	char *ResultCString = Result.ToCString();
+	NSString *ResultString = [[NSString alloc] initWithUTF8String: ResultCString];
+	delete[] ResultCString;
+	[self.output setStringValue: ResultString];
 }
 @end
