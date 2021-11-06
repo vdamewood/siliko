8f22270a87e248b488d967a271dee005805f6e71
Vincent Damewood <git@vdamewood.com>
2021-11-06 11:53:48 -0700

    Update FindReadline

--- cmake/FindReadline.cmake
+++ cmake/FindReadline.cmake
@@ -1,5 +1,5 @@
 # FindReadline.cmake: Find Readline
-# Copyright 2019 Vincent Damewood
+# Copyright 2020 Vincent Damewood
 #
 # This program is free software: you can redistribute it and/or modify
 # it under the terms of the GNU General Public License as published by
@@ -14,19 +14,27 @@
 # You should have received a copy of the GNU General Public License
 # along with this program. If not, see <http://www.gnu.org/licenses/>.
 
-find_library(READLINE_LIB readline)
-find_path(READLINE_INC NAMES readline/readline.h)
-if(READLINE_LIB AND READLINE_INC)
-	add_library(Readline SHARED IMPORTED GLOBAL)
-	add_library(Readline::Readline ALIAS Readline)
-	set_property(TARGET Readline PROPERTY IMPORTED_LOCATION "${READLINE_LIB}")
-	target_include_directories(Readline INTERFACE "${READLINE_INC}")
+message(STATUS "Finding Readline")
+
+find_library(Readline_LIBRARY readline)
+if(Readline_LIBRARY)
+	message(STATUS "  Readline library found at ${Readline_LIBRARY}")
+else()
+	message(STATUS "  Readline library not found.")
+endif()
+
+find_path(Readline_INCLUDE_DIR NAMES readline/readline.h)
+if(Readline_INCLUDE_DIR)
+	message(STATUS "  Readline includes found at ${Readline_INCLUDE_DIR}")
+else()
+	message(STATUS "  Readline includes not found.")
+endif()
+
+if(Readline_LIBRARY AND Readline_INCLUDE_DIR)
+	add_library(Readline::Readline UNKNOWN IMPORTED GLOBAL)
+	set_property(TARGET Readline::Readline PROPERTY IMPORTED_LOCATION "${Readline_LIBRARY}")
+	target_include_directories(Readline::Readline INTERFACE "${Readline_INCLUDE_DIR}")
 	set(Readline_FOUND 1)
 else()
 	set(Readline_FOUND 0)
 endif()
-
-mark_as_advanced(
-	READLINE_INC
-	READLINE_LIB
-)
