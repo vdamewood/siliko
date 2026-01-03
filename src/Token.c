4f8f9a67f8a2b7d2351018e4fd69ea3d32043a6c
Vincent Damewood <git@vdamewood.com>
2026-01-02 19:48:10 -0800

    Update copyright

--- src/Token.c
+++ src/Token.c
@@ -1,19 +1,22 @@
-/* Token.c: Token used by lexical analyzer
- * Copyright 2012-2025 Vincent Damewood
- *
- * This library is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
- * the Free Software Foundation, either version 3 of the License, or
- * (at your option) any later version.
- *
- * This library is distributed in the hope that it will be useful,
- * but WITHOUT ANY WARRANTY; without even the implied warranty of
- * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the
- * GNU Lesser General Public License for more details.
- *
- * You should have received a copy of the GNU Lesser General Public License
- * along with this library. If not, see <http://www.gnu.org/licenses/>.
- */
+// Copyright 2012-2026 Vincent Damewood
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+// This file is part of Siliko.
+
+// Siliko is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published 
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Siliko is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with Siliko. If not, see
+// <http://www.gnu.org/licenses/>.
+
 
 #include <SilikoCore/Token.h>
 
