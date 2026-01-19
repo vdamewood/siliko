40d25f4ea2e2b04adeafbd58cca5ba789c867792
Vincent Damewood <git@vdamewood.com>
2026-01-19 02:32:46 -0800

    Add all-inclusive header

---
+++ include/SilikoCore/SilikoCore.h
@@ -0,0 +1,36 @@
+// Copyright 2012-2026 Vincent Damewood
+// SPDX-License-Identifier: LGPL-3.0-or-later
+
+// This file is part of Siliko.
+
+// Siliko is free software: you can redistribute it and/or modify it
+// under the terms of the GNU Lesser General Public License as published
+// by the Free Software Foundation, either version 3 of the License, or
+// (at your option) any later version.
+
+// Siliko is distributed in the hope that it will be useful, but
+// WITHOUT ANY WARRANTY; without even the implied warranty of
+// MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE. See the GNU
+// Lesser General Public License for more details.
+
+// You should have received a copy of the GNU Lesser General Public
+// License along with Siliko. If not, see
+// <http://www.gnu.org/licenses/>.
+
+#if !defined SILIKO_CORE_SILIKO_CORE_H
+#define SILIKO_CORE_SILIKO_CORE_H
+
+#include <SilikoCore/Api.h>
+#include <SilikoCore/Evaluate.h>
+#include <SilikoCore/Function.h>
+#include <SilikoCore/Input.h>
+#include <SilikoCore/Lexer.h>
+#include <SilikoCore/Math.h>
+#include <SilikoCore/Node.h>
+#include <SilikoCore/Operator.h>
+#include <SilikoCore/ParseInfix.h>
+#include <SilikoCore/Token.h>
+#include <SilikoCore/Value.h>
+#include <SilikoCore/Version.h>
+
+#endif // SILIKO_CORE_SILIKO_CORE_H
